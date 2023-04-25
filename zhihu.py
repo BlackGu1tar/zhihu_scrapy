0b1120fe7d42e0425953b8da311345e16e2a2bbe
BlackGu1tar <kgog0011@gmail.com>
2023-04-25 16:43:54 +0800

    Update zhihu.py

--- zhihu.py
+++ zhihu.py
@@ -7,7 +7,7 @@ import time,re
 
 #f=open('zhihu.txt','w',encoding='utf-8')
 url_answers='https://www.zhihu.com/api/v4/questions/377861079/feeds?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Cis_labeled%2Cpaid_info%2Cpaid_info_content%2Creaction_instruction%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B*%5D.mark_infos%5B*%5D.url%3Bdata%5B*%5D.author.follower_count%2Cvip_info%2Cbadge%5B*%5D.topics%3Bdata%5B*%5D.settings.table_of_content.enabled&offset=&limit=3&order=default&platform=desktop'
-
+img_urls = []
 while True:
     headers = {"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1.6) ",
                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -25,7 +25,6 @@ while True:
         content_answers = data_answers['target']['content']
         imgs_content = re.findall(r'https:\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?',content_answers)
         tmp_img_urls = set([later_url for _, later_url in imgs_content])
-        img_urls = []
         for x in tmp_img_urls:
             if '_r.jpg' in x:
                 img_urls.append(f"https://picx.zhimg.com{x}")
